32c6fd10391d6b3d0d31b58bbee6d0e46b4df8ac
Daniil Poliakov <d.poliakov@yahoo.com>
2021-02-01 15:33:50 +0600

    table created

---
+++ academrecycle/src/components/point/Point.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+import './style.css';
+
+export default class Point extends React.Component {
+
+    render() {
+
+        const {id, lat, lng, title, content_text, address} = this.props;
+
+        return (
+            <article className="point">
+                <div className="id">{id}</div>
+                <div className="coordinates">{lat}N {lng}E</div>
+                <div className="title">{title}</div>
+                <div className="content">{content_text}</div>
+                <div className="address">{address}</div>
+            </article>
+        );
+    };
+};
---
+++ academrecycle/src/pages/table/Table.js
@@ -0,0 +1,35 @@
+import React from "react";
+import pointsFromJson from './recyclePoints.json'
+import Point from "../../components/point/Point";
+import "./style.css";
+
+class Table extends React.Component {
+
+    renderList = () => {
+        let pointList = []
+        for (const [, value] of Object.entries(pointsFromJson)) {
+            pointList.push(value)
+        }
+
+        return <div className={'point-list'}>
+            {
+                pointList.map((item) => {
+                    return (
+                        <Point key id={item.id} lat={item.lat} lng={item.lng} title={item.title}
+                               content_text={item.content_text} address={item.address}/>
+                    );
+                })
+            }
+        </div>;
+    };
+
+    render() {
+        return (
+            <section className='main__content'>
+                {this.renderList()}
+            </section>
+        );
+    }
+}
+
+export default Table;
