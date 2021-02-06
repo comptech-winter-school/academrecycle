a0008b0660754a01946da35c6939c4fc4e5a42f7
Oleg <30134150+NuMB2014@users.noreply.github.com>
2021-02-06 10:21:55 +0700

    changed script and migration for integer type

--- backend/migrations/20210203062417-create-recycle-type.js
+++ backend/migrations/20210203062417-create-recycle-type.js
@@ -26,7 +26,7 @@ module.exports = {
             },
             type: {
                 allowNull: false,
-                type: Sequelize.STRING
+                type: Sequelize.INTEGER
             },
             createdAt: {
                 allowNull: false,
